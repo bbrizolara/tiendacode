0390507d94aa9234975a08fbd9521629086cb35a
bbrizolara <bbrizolara@codigodelsur.com>
2021-09-28 17:21:58 -0300

    Adding index to user email

--- db/migrate/20210927232543_create_users.rb
+++ db/migrate/20210927232543_create_users.rb
@@ -2,7 +2,7 @@ class CreateUsers < ActiveRecord::Migration[6.1]
   def change
     create_table :users do |t|
       t.string :name, null: false
-      t.string :email, null: false
+      t.string :email, index: { unique: true }
       t.string :password_digest, null: false
       t.boolean :active, null: false, default: false
 
--- db/schema.rb
+++ db/schema.rb
@@ -50,11 +50,12 @@ ActiveRecord::Schema.define(version: 2021_09_27_232543) do
 
   create_table "users", force: :cascade do |t|
     t.string "name", null: false
-    t.string "email", null: false
+    t.string "email"
     t.string "password_digest", null: false
     t.boolean "active", default: false, null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
