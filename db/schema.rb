0d92a2b93bca75171f24efe16c8d6f28a85a6636
bbrizolara <bbrizolara@codigodelsur.com>
2021-09-28 17:23:33 -0300

    Adding nullable false to user email

--- db/schema.rb
+++ db/schema.rb
@@ -50,7 +50,7 @@ ActiveRecord::Schema.define(version: 2021_09_27_232543) do
 
   create_table "users", force: :cascade do |t|
     t.string "name", null: false
-    t.string "email"
+    t.string "email", null: false
     t.string "password_digest", null: false
     t.boolean "active", default: false, null: false
     t.datetime "created_at", precision: 6, null: false
