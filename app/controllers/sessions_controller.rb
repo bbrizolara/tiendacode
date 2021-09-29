e818c21280eb741c5d81a63b086ad67d2a176c62
bbrizolara <bbrizolara@codigodelsur.com>
2021-09-29 13:14:50 -0300

    Adding Login/Logout, Navbar and Flash Partial

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,29 @@
+class SessionsController < ApplicationController
+  include SessionsHelper
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    if user && user.authenticate(params.dig(:session, :password))
+      log_in user
+      flash.now[:notice] = "Logged in successfully"
+      redirect_to user
+    else
+      flash.now[:alert] = "Invalid credentials"
+      render :new
+    end
+  end
+
+  def destroy
+    log_out
+    redirect_to root_path
+  end
+
+  private
+
+  def user
+    @user ||= User.find_by(email: params.dig(:session, :email).downcase)
+  end
+end
