9d2090a76b1b91e3f1659802c83dc6d2ebf9942d
bbrizolara <bbrizolara@codigodelsur.com>
2021-10-07 13:22:43 -0300

    Add presenter option to aprams

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
-  def present(model)
-    klass = "#{model.class}Presenter".constantize
+  def present(model, presenter_class = nil)
+    klass = presenter_class || "#{model.class}Presenter".constantize
     presenter = klass.new(model, self)
     yield(presenter) if block_given?
   end
