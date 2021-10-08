ebbc4f794f7a59cbbbf8a65c4992e362d0970d2a
bbrizolara <bbrizolara@codigodelsur.com>
2021-10-08 11:04:57 -0300

    Refactoring activation method

--- app/controllers/account_activations_controller.rb
+++ app/controllers/account_activations_controller.rb
@@ -12,18 +12,22 @@ class AccountActivationsController < ApplicationController
         account_activated = true        
       end
     end
-    if account_activated
-      flash[:notice] = "Account activated successfully"
-    else
-      flash.now[:alert] = "Invalid activation link"
-    end    
-    redirect_to root_path
+    handle_redirect(account_activated)
   end
 
   private
 
   def user
     user = User.find_by(email: params.dig(:email))
+  end
+
+  def handle_redirect(account_activated)
+    if account_activated
+      flash[:notice] = "Account activated successfully"
+    else
+      flash[:alert] = "Invalid activation link"
+    end    
+    redirect_to root_path
   end
 
   def authenticated?(attribute, token)
