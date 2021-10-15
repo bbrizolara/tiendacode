553a4d3ba033d0872107468c29a84d2428090fce
bbrizolara <bbrizolara@codigodelsur.com>
2021-10-15 15:47:26 -0300

    Show/Hide favorite product description

--- app/javascript/controllers/favorite_products_controller.js
+++ app/javascript/controllers/favorite_products_controller.js
@@ -1,18 +1,34 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = ['favorite']
-
-  connect(){
-    console.log('connected')
-  }
+  static targets = ['favorite', 'item']
+  static values = { index: Number }
 
   add_remove(event) {
     const [data, status, xhr] = event.detail;
     this.favoriteTarget.innerHTML = xhr.response;   
-  }
+  };
+
+  hideShowHandler(event){
+    const params = event.params;
+    if (this.indexValue == params.index) {
+      this.indexValue = null;
+    } else {      
+      this.indexValue = params.index;
+    }
+  };
+
+  indexValueChanged(){
+    if (this.loadChanges){
+      this.itemTargets.forEach((element, index) => {
+      element.hidden = index != this.indexValue
+    });
+    } else {
+      this.loadChanges= true;
+    }
+  };
 
   error(){
-    console.log('error');
-  }
+    console.log('Ajax Error');
+  };
 }
