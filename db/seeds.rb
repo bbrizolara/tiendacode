1ef9ffbf7a3db173da1d4914022b99719bfddbf4
bbrizolara <bbrizolara@codigodelsur.com>
2021-10-19 14:27:23 -0300

    Seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
 roles = Role.create([{ name: 'Admin' }, { name: 'User' }])
 users = User.create(name: 'Admin', email: 'bbrizolara@cds.com', password: 'password', 
-                    password_confirmation: 'password', active: true, role: roles.first)
+                    password_confirmation: 'password', active: true, role: roles.first,
+                    activated_at: Time.current)
